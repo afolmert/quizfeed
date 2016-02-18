a2f00e08192fe776dff5e4b4d1b328f046a7608a
Adam Folmert <afolmert@gmail.com>
2016-02-18 13:17:59 +0100

    Fixed formatting

--- app.ts
+++ app.ts
@@ -32,23 +32,23 @@ function testLoadEntries(filepath: string) {
 
 function processFiles(filepaths: string[], options: any) {
 
-	var entries: quizfeed.Entry[] = [];
+    var entries: quizfeed.Entry[] = [];
 
-	filepaths.forEach(filepath => {
-		var tmp: quizfeed.Entry[] = quizfeed.Entries.loadEntries(filepath);
-		entries = entries.concat(tmp);
-	})
-	console.log('Loaded Entries ------------------ ');
+    filepaths.forEach(filepath => {
+        var tmp: quizfeed.Entry[] = quizfeed.Entries.loadEntries(filepath);
+        entries = entries.concat(tmp);
+    })
+    console.log('Loaded Entries ------------------ ');
     console.log(entries);
 
-	if (options.shuffle) {
-		utils.shuffleArray(entries);
-	} else if (options.sort) {
-		entries.sort((a, b) => a.question.localeCompare(b.question));
-	}
+    if (options.shuffle) {
+        utils.shuffleArray(entries);
+    } else if (options.sort) {
+        entries.sort((a, b) => a.question.localeCompare(b.question));
+    }
 
-	console.log('Output is --------------------------- ');
--	console.log(quizfeed.Entries.exportEntries(entries));
+    console.log('Output is --------------------------- ');
+    -	console.log(quizfeed.Entries.exportEntries(entries));
 
 }
 
@@ -60,15 +60,15 @@ var argv = require('yargs')
     .usage('Usage: quizfeed [options] [file...]')
     .example('quizfeed --swap --title "German words" file.txt', 'generates quiz for entries in given file.txt')
     .alias('s', 'swap')
-	.describe('s', 'Swap questions with answers')
-	.alias('t', 'title')
-	.describe('t', 'Sets title for all generated entries')
+    .describe('s', 'Swap questions with answers')
+    .alias('t', 'title')
+    .describe('t', 'Sets title for all generated entries')
     .help('h')
     .alias('h', 'help')
-	.alias('v', 'version')
-	.describe('v', 'Shows quizfeed version')
-	.describe('shuffle', 'Shuffle output order')
-	.describe('sort', 'Sort output order')
+    .alias('v', 'version')
+    .describe('v', 'Shows quizfeed version')
+    .describe('shuffle', 'Shuffle output order')
+    .describe('sort', 'Sort output order')
     .argv;
 
 
@@ -77,13 +77,13 @@ var argv = require('yargs')
 if (argv.v) {
     var p = require('./package.json');
     console.log('version ' + p.version);
-	process.exit(1);
+    process.exit(1);
 }
 
 
 if (!argv._ || argv._.length == 0) {
-	console.log('No input files');
-	process.exit(1);
+    console.log('No input files');
+    process.exit(1);
 }
 
 
@@ -91,10 +91,10 @@ if (!argv._ || argv._.length == 0) {
 var filepaths: string[] = [];
 
 argv._.forEach(arg => {
-	if (!fs.existsSync(arg)) {
-		console.log(arg + ' is not a file');
-		process.exit(1);
-	}
+    if (!fs.existsSync(arg)) {
+        console.log(arg + ' is not a file');
+        process.exit(1);
+    }
 });
 
 
--- lib/quizfeed.ts
+++ lib/quizfeed.ts
@@ -51,38 +51,38 @@ export class Option {
 
     static parse(input: string): Option {
 
-		var result: Option = new Option();
+        var result: Option = new Option();
 
         if (utils.isNullOrWhitespace(input)) {
             throw new Error('Non-empty input expected');
         }
 
-		input = input.trim();
+        input = input.trim();
 
         if (!utils.startsWith(input, '#![') || !utils.endsWith(input, ']')) {
             throw new Error('Invalid syntax: ' + input);
         }
 
-		input = input.substr(3, input.length - 4);
+        input = input.substr(3, input.length - 4);
 
-		if (input.indexOf('(') >= 0) {
+        if (input.indexOf('(') >= 0) {
 
-	        var regexp: RegExp = /^([a-zA-Z]+)\((.+)\)$/;
+            var regexp: RegExp = /^([a-zA-Z]+)\((.+)\)$/;
 
-			var match = regexp.exec(input);
+            var match = regexp.exec(input);
 
-			if (match != null) {
-				result.name = match[1];
-				result.value = match[2];
-			} else {
-				throw new Error('Cannot parse option: ' + input);
-			}
+            if (match != null) {
+                result.name = match[1];
+                result.value = match[2];
+            } else {
+                throw new Error('Cannot parse option: ' + input);
+            }
 
-		} else {
+        } else {
             result.name = input;
             result.value = null;
 
-		}
+        }
         return result;
 
     }
@@ -94,18 +94,18 @@ export class Option {
 export class Context {
     public shouldSwap: boolean;
     public separator: RegExp;
-	public title: string;
+    public title: string;
 
     constructor() {
         this.shouldSwap = false;
         this.separator = /\t/;
-		this.title = '';
+        this.title = '';
     }
 
-	static isContextLine(input: string): boolean {
-		return false;
+    static isContextLine(input: string): boolean {
+        return false;
 
-	}
+    }
 
     static parseSeparator(input: string): RegExp {
 
@@ -120,21 +120,21 @@ export class Context {
 
     }
 
-	update(input: string) {
+    update(input: string) {
 
-		var option: Option = Option.parse(input);
+        var option: Option = Option.parse(input);
 
-		switch (option.name) {
-			case OPTION_TITLE:
-				this.title = utils.stripQuotes(option.value);
-				break;
-			case OPTION_SEPARATOR:
-				this.separator = Context.parseSeparator(option.value);
-				break;
-			case OPTION_SHOULD_SWAP:
-				this.shouldSwap = JSON.parse(option.value);
-		}
-	}
+        switch (option.name) {
+            case OPTION_TITLE:
+                this.title = utils.stripQuotes(option.value);
+                break;
+            case OPTION_SEPARATOR:
+                this.separator = Context.parseSeparator(option.value);
+                break;
+            case OPTION_SHOULD_SWAP:
+                this.shouldSwap = JSON.parse(option.value);
+        }
+    }
 
 
     toString(): string {
@@ -148,67 +148,67 @@ export class Context {
 
 
 export class Entry {
-	public title: string;
-	public question: string;
-	public answer: string;
-	public shouldSwap: boolean;
+    public title: string;
+    public question: string;
+    public answer: string;
+    public shouldSwap: boolean;
 
-	constructor(question?: string, answer?: string) {
-		this.question = question;
-		this.answer = answer;
-	}
-
-
-	static parse(input: string, context: Context): Entry {
-
-		var parts: string[] = input.split(context.separator);
-
-		if (parts.length != 2) {
-			throw new Error('Error parsing input ' + input + ' parsed parts are ' + JSON.stringify(parts, null, 2));
-		}
-
-		var result: Entry = new Entry();
-		result.question = parts[0];
-		result.answer = parts[1];
-		result.title = context.title.slice(0);
-		result.shouldSwap = context.shouldSwap;
+    constructor(question?: string, answer?: string) {
+        this.question = question;
+        this.answer = answer;
+    }
 
 
-		return result;
+    static parse(input: string, context: Context): Entry {
 
-	}
+        var parts: string[] = input.split(context.separator);
 
-	toString() {
-		return `Entry: ${this.title}: ${this.question} ${this.answer} SWAP: ${this.shouldSwap}`;
-	}
+        if (parts.length != 2) {
+            throw new Error('Error parsing input ' + input + ' parsed parts are ' + JSON.stringify(parts, null, 2));
+        }
+
+        var result: Entry = new Entry();
+        result.question = parts[0];
+        result.answer = parts[1];
+        result.title = context.title.slice(0);
+        result.shouldSwap = context.shouldSwap;
+
+
+        return result;
+
+    }
+
+    toString() {
+        return `Entry: ${this.title}: ${this.question} ${this.answer} SWAP: ${this.shouldSwap}`;
+    }
 }
 
 
 
 export class Entries {
 
-	static loadLines(filepath: string): string[] {
-		var content: string = fs.readFileSync(filepath, 'utf8');
+    static loadLines(filepath: string): string[] {
+        var content: string = fs.readFileSync(filepath, 'utf8');
 
-		var lines: string[] = content.split(/\r\n|\n/);
-		return lines;
-	}
+        var lines: string[] = content.split(/\r\n|\n/);
+        return lines;
+    }
 
 
 
-	static loadEntries(filepath: string): Entry[] {
+    static loadEntries(filepath: string): Entry[] {
 
-		var context: Context = new Context();
+        var context: Context = new Context();
 
-		if (!filepath) {
-			throw new Error('filepath cannot be empty');
-		}
-		if (!fs.existsSync(filepath)) {
-			throw new Error(`filepath ${filepath} is not a file`);
-		}
+        if (!filepath) {
+            throw new Error('filepath cannot be empty');
+        }
+        if (!fs.existsSync(filepath)) {
+            throw new Error(`filepath ${filepath} is not a file`);
+        }
 
-		var result: Entry[] = [];
-		var lines: string[] = this.loadLines(filepath);
+        var result: Entry[] = [];
+        var lines: string[] = this.loadLines(filepath);
 
         for (var i = 0; i < lines.length; i++) {
             try {
@@ -230,37 +230,37 @@ export class Entries {
                 error(filepath, i, e.message);
             }
         }
-		return result;
-	}
+        return result;
+    }
 
 
-	static buildExportLine(title: string, question: string, answer: string): string {
-		var result: string = '';
-		if (!utils.isNullOrEmpty(title)) {
-			result = `${title}: ${question}\t${answer}`;
-		} else {
-			result = `${question}\t${answer}`;
-		}
-		return result;
-	}
+    static buildExportLine(title: string, question: string, answer: string): string {
+        var result: string = '';
+        if (!utils.isNullOrEmpty(title)) {
+            result = `${title}: ${question}\t${answer}`;
+        } else {
+            result = `${question}\t${answer}`;
+        }
+        return result;
+    }
 
-	static exportEntries(entries: Entry[]): string {
+    static exportEntries(entries: Entry[]): string {
 
-		var result: string = '';
+        var result: string = '';
 
-		entries.forEach(entry => {
+        entries.forEach(entry => {
 
-			result += this.buildExportLine(entry.title, entry.question, entry.answer) + '\n';
+            result += this.buildExportLine(entry.title, entry.question, entry.answer) + '\n';
 
-			if (entry.shouldSwap) {
-				result += this.buildExportLine(entry.title, entry.answer, entry.question) + '\n';
-			}
+            if (entry.shouldSwap) {
+                result += this.buildExportLine(entry.title, entry.answer, entry.question) + '\n';
+            }
 
-		});
+        });
 
-		return result;
+        return result;
 
 
-	}
+    }
 
 }
